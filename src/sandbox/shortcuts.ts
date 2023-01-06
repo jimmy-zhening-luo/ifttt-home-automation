0c7a742c9fabd728cb4b7fb4e396f063229300ab
Jimmy Zhening Luo <jimmy-zhening-luo@users.noreply.github.com>
2023-01-06 15:04:59 -0800

    WIP: untested: writing a test handler for IFTTT

---
+++ src/sandbox/shortcuts.ts
@@ -0,0 +1,46 @@
+type Moment = moment.Moment;
+
+interface TimeStamp<Type> {
+  readonly event: string;
+  readonly time: Type;
+}
+
+function formatMoment (_moment: Moment): string {
+  return _moment.toString() as string;
+}
+
+
+const times: Array<TimeStamp<Moment> > = new Array<TimeStamp<Moment> >();
+
+times.push({
+  event: "iOS Shortcut Invoked",
+  time: moment(IosShortcuts.shortcutAutomationStarted.OccurredAt as string) as Moment
+  });
+
+times.push({
+  event: "IFTTT Applet Began Running",
+  time: Meta.triggerTime as Moment
+  });
+
+times.push({
+  event: "Current Runtime",
+  time: Meta.currentUserTime as Moment
+  });
+
+/*
+times.push({
+  event: "iRobot Polled: Last Clean Start",
+  time: moment(Irobot.historyOfRobotStarted[0].Timestamp as string) as Moment
+});
+*/
+
+const printableTimes: Array<TimeStamp<string> > = times.map((t) => ({
+  "event": t.event as string,
+  "time": formatMoment(t.time as Moment) as string
+  }));
+
+const alertMessageLines: Array<string> = printableTimes.map((t) => [ t.event as string, t.time as string ].join(": "));
+
+const alertMessage: string = alertMessageLines.join("\n");
+
+IfNotifications.sendNotification.setMessage(alertMessage as string);
